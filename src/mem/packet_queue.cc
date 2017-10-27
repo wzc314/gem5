09dd5098bcaca5ec93961cadff967eada915ad45
Kris Wang <wangzicong@nudt.edu.cn>
2017-10-27 19:15:49 +0800

    Disable sanity check.

--- src/mem/packet_queue.cc
+++ src/mem/packet_queue.cc
@@ -56,6 +56,7 @@ PacketQueue::PacketQueue(EventManager& _em, const std::string& _label,
       _disableSanityCheck(disable_sanity_check),
       label(_label), waitingOnRetry(false)
 {
+    disableSanityCheck();
 }
 
 PacketQueue::~PacketQueue()
